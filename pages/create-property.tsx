4259d9d21c16a457e1dd36acfb1db2cc1527c2d4
Svetoslav99 <52759406+Svetoslav99@users.noreply.github.com>
2022-10-23 17:22:08 +0300

    Minor: Added nextjs 'Head' component for some meta data for better SEO.

--- pages/create-property.tsx
+++ pages/create-property.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Head from 'next/head';
 
 import { CreateProperty } from '../components';
 
-const Index = () => {
-    return <CreateProperty />;
+const Index: React.FC = () => {
+    return (
+        <>
+            <Head>
+                <title>Create property</title>
+                <meta name='description' content='Create new property page.' />
+            </Head>
+            <CreateProperty />
+        </>
+    );
 };
 
 export default Index;
