c7709865228804ba69e56096e3e137e5144e3e4c
Svetoslav99 <52759406+Svetoslav99@users.noreply.github.com>
2022-10-23 17:11:29 +0300

    Minor: Added nextjs 'Head' component for some meta data for better SEO.

--- pages/property-details.tsx
+++ pages/property-details.tsx
@@ -1,9 +1,18 @@
+import Head from 'next/head';
 import React from 'react';
 
 import { DetailsProperty } from '../components';
 
-const Index = () => {
-    return <DetailsProperty />;
+const Index: React.FC = () => {
+    return (
+        <>
+            <Head>
+                <title>Property Details</title>
+                <meta name='description' content='Property details page based on provided plan number.' />
+            </Head>
+            <DetailsProperty />
+        </>
+    );
 };
 
 export default Index;
