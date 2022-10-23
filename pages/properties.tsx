cd45a265d52716a11403faa66686edea57cf3025
Svetoslav99 <52759406+Svetoslav99@users.noreply.github.com>
2022-10-23 17:12:11 +0300

    Minor: Added nextjs 'Head' component for some meta data for better SEO.
    Minor: Removed forgotten comments.

--- pages/properties.tsx
+++ pages/properties.tsx
@@ -1,36 +1,21 @@
 import React from 'react';
-import { ViewProperty } from '../types/property';
+import Head from 'next/head';
 
 import { Properties } from '../components';
 
 const Index: React.FC = () => {
-    return <Properties />;
+    return (
+        <>
+            <Head>
+                <title>Properties</title>
+                <meta
+                    name='description'
+                    content='Properties page. From here you can search for a specific property or to delete a property.'
+                />
+            </Head>
+            <Properties />
+        </>
+    );
 };
 
 export default Index;
-
-// import React from 'react';
-// import { ViewProperty } from '../types/property';
-
-// import { Properties } from '../components';
-
-// type Props = {
-//     properties: ViewProperty[];
-// };
-
-// const Index: React.FC<Props> = ({ properties }) => {
-//     return <Properties allProperties={properties} />;
-// };
-
-// export default Index;
-
-// export async function getServerSideProps() {
-//     const res = await fetch(`${process.env.HOST_ADDRESS}/api/properties`);
-//     const { data } = await res.json();
-
-//     return {
-//         props: {
-//             properties: data
-//         }
-//     };
-// }
